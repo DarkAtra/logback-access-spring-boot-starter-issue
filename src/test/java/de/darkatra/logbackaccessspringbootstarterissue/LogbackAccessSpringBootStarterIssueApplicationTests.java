a753350d406c8d92d56cdd7e854e8dd6bc7467ff
Tommy Schmidt <tommy.schmidt@idealo.de>
2024-08-16 18:37:54 +0200

    chore: reproducer project

---
+++ src/test/java/de/darkatra/logbackaccessspringbootstarterissue/LogbackAccessSpringBootStarterIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package de.darkatra.logbackaccessspringbootstarterissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbackAccessSpringBootStarterIssueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
